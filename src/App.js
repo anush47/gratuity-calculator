238a8967494bc17fa7bad9c24dc35c46899f8f32
anush12345 <75567234+anush12345@users.noreply.github.com>
2024-02-20 04:02:09 +0530

    changed back to browserrouter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 
 function App() {
   return (
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Homepage />}></Route>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
