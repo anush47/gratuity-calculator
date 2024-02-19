11d0d30e738bce1b10ce9e5eb5401936e32952a4
anush12345 <75567234+anush12345@users.noreply.github.com>
2024-02-20 03:52:13 +0530

    Changed to Hash router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Homepage />}></Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
